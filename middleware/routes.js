23c6d048ea52132fa9808138380025250d9ae7e2
budarin <budarin@stardreams.co>
2016-09-25 22:12:42 +0300

    db call

--- middleware/routes.js
+++ middleware/routes.js
@@ -1,14 +1,55 @@
 import Router from 'koa-router';
 import convert from 'koa-convert';
 import KoaBody from 'koa-body';
+import pg from 'pg';
+
+var config = {
+    user: 'postgres', //env var: PGUSER
+    database: 'kometa_db', //env var: PGDATABASE
+    password: 'wwwboy123', //env var: PGPASSWORD
+    host: 'localhost', // Server hosting the postgres database
+    port: 5432, //env var: PGPORT
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+};
 
 const
     router = new Router(),
-    koaBody = convert(KoaBody());
+    koaBody = convert(KoaBody()),
+    pool = new pg.Pool(config);
+
+pool.on('error', function (err, client) {
+    // if an error is encountered by a client while it sits idle in the pool
+    // the pool itself will emit an error event with both the error and
+    // the client which emitted the original error
+    // this is a rare occurrence but can happen if there is a network partition
+    // between your application and the database, the database restarts, etc.
+    // and so you might want to handle it and at least log it out
+    console.error('idle client error', err.message, err.stack);
+    throw err;
+});
 
 router
     .get('/', async (ctx, next) => {
-        ctx.body = 'Hello World';
+        const p = await new Promise((resolve, reject) => {
+            pool.connect(function(err, client, done) {
+                if(err) {
+                    //return console.error('error fetching client from pool', err);
+                    return reject(err);
+                }
+                client.query('SELECT now()', function(err, result) {
+                    //call `done()` to release the client back to the pool
+                    done();
+
+                    if(err) {
+                        return reject(err);
+                    }
+                    resolve(result.rows[0]);
+                });
+            });
+        });
+
+        ctx.body = p.now.toISOString();
     })
     .get('/foo', async (ctx, next) => {
         ctx.body = 'Hello World Foo';
