9dfad3fb0c1cc427274b9976c91e1eac991019d6
budarin <budarin@stardreams.co>
2016-09-25 23:04:39 +0300

    db call sample query

--- middleware/routes.js
+++ middleware/routes.js
@@ -31,13 +31,14 @@ pool.on('error', function (err, client) {
 
 router
     .get('/', async (ctx) => {
-        const p = await new Promise((resolve, reject) => {
+        const
+            result = await new Promise((resolve, reject) => {
             pool.connect(function(err, client, done) {
                 if(err) {
                     //return console.error('error fetching client from pool', err);
                     return reject(err);
                 }
-                client.query('SELECT * FROM pg_catalog.pg_tables', function(err, result) {
+                client.query('SELECT tablename FROM pg_catalog.pg_tables', function(err, result) {
                     //call `done()` to release the client back to the pool
                     done();
 
@@ -47,9 +48,14 @@ router
                     resolve(result);
                 });
             });
-        });
+        }),
+        getTableNames = (result, item) => {
+            result.push(item.tablename);
+            return result;
+        };
 
-        ctx.body = p.rows;
+        ctx.body = result.rows.reduce(getTableNames, []);
+
     })
     .get('/foo', async (ctx) => {
         ctx.body = 'Hello World Foo';
