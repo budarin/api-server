88affef2b14a1d362dd7839aba3cb981d667da8c
budarin-vv@yandex.ru <u3s9aSttjR>
2016-10-23 03:28:08 +0400

    add OAuthServer

--- middleware/routes.js
+++ middleware/routes.js
@@ -8,32 +8,34 @@ import pgPoolConfig from '../config/pgPool.config';
 
 const
     router = new Router(),
-    { welcomeRoute, getRoute, postRoute, putRoute, deleteRoute } = apiRoutes,
-
     pool = pgPoolConfig(pg),
+    routes = () => router.routes(),
+    allowedMethods = () => router.allowedMethods(),
+
     oneMonth = 1000 * 60 * 60 * 24 * 30,
     sendFavicon = sendFile('public/favicon.ico', oneMonth),
     sendRobotsTxt = sendFile('public/robots.txt', oneMonth),
 
-    routes = () => router.routes(),
-    allowedMethods = () => router.allowedMethods(),
+    { welcomeRoute, getRoute, postRoute, putRoute, deleteRoute } = apiRoutes,
+
     initRoutes = app => {
-        const { OAuthServer } = app;
+        const
+            parser = convert(koaBodyParser()),
+            grant = convert(app.OAuthServer.grant()),
+            authorize = convert(app.OAuthServer.authorise());
 
         router
-            .post('/oauth2/token/', convert(koaBodyParser()), convert(OAuthServer.grant()));
+            .post('/oauth2/token/', parser, grant)
 
-        router
             .get('/', welcomeRoute)
             .get('/favicon.ico', sendFavicon)
-            .get('/robots.txt', sendRobotsTxt);
+            .get('/robots.txt', sendRobotsTxt)
 
-        router
-            .get('/api/v1/', welcomeRoute)
-            .get('/api/v1/:entity/:method/', getRoute(pool))
-            .post('/api/v1/:entity/', postRoute(pool))
-            .put('/api/v1/:entity/', putRoute(pool))
-            .delete('/api/v1/:entity/', deleteRoute(pool));
+            .get('/api/v1/', authorize, welcomeRoute)
+            .get('/api/v1/:entity/:method/', authorize, getRoute(pool))
+            .post('/api/v1/:entity/', authorize, postRoute(pool))
+            .put('/api/v1/:entity/', authorize, putRoute(pool))
+            .delete('/api/v1/:entity/', authorize, deleteRoute(pool));
 
         return {
             routes,
