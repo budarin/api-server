f1120ede956f65766946bd34d433dbe4c1559a98
vadim_budarin <budarin-vv@yandex.ru>
2016-10-24 05:02:24 +0300

    first test

--- config/server.config.js
+++ config/server.config.js
@@ -1,4 +1,16 @@
 const serverConfig = {
+    'test': {
+        'port': 3000,
+        'pg_config': {
+            'database': 'kometa_db',
+            'user': 'api_user',
+            'password': 'password123',
+            'host': 'localhost',
+            'port': 5432,
+            'max': 50,
+            'idleTimeoutMillis': 30000
+        }
+    },
     'development': {
         'port': 3000,
         'pg_config': {
---
+++ test/test.js
@@ -0,0 +1,11 @@
+var
+    request = require('supertest'),
+    app = require(__dirname + '/../server');
+
+describe('GET /', function () {
+    it('should contain text "Hello, Express!"', function (done) {
+        request(app)
+            .get('/')
+            .expect(/Hello, Express!/, done)
+    })
+});
