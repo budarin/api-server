6874636afd20cfe7126f7d1fb1f626c6bd8a5912
vadim_budarin <budarin-vv@yandex.ru>
2016-10-24 05:06:29 +0300

    first test

--- test/test.js
+++ test/test.js
@@ -2,6 +2,10 @@ var
     request = require('supertest'),
     app = require(__dirname + '/../server');
 
+app.address = function () {
+    return 'http://localhost:3000';
+};
+
 describe('GET /', function () {
     it('should contain text "Hello, Express!"', function (done) {
         request(app)
